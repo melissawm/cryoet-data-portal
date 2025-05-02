3ad7485bd618355352eda1ce6fc287de36e4965b
Melissa Weber Mendonça <melissawm@gmail.com>
2025-05-02 15:38:52 -0300

    update pages

--- dev/_static/js/version_redirect.js
+++ dev/_static/js/version_redirect.js
@@ -1,31 +1,11 @@
 window.addEventListener("DOMContentLoaded", function () {
-  versions = document.querySelectorAll(".md-version__list");
-  // Change every item in the list to point to the current page's url with the proper version
+  versions = document.querySelectorAll(".md-version__item");
+  const currentUrl = window.location.href;
+  var currentVersion = currentUrl.match(/\/v\d+\.\d+(\-?rc\d+)?|dev\//g)[0];
+  var splitUrl = currentUrl.split(currentVersion);
   versions.forEach((version) => {
-    const currentVersion = window.location.href.split("/").slice(-2, -1)[0];
-    console.log("Current version=", currentVersion);
-    const selectedVersion = version.querySelector(".md-version__item").value;
-    console.log("Selected version=", selectedVersion);
-    const redirectUrl = window.location.href.replace(
-      `/${currentVersion}/`,
-      `/${selectedVersion}/`
-    );
-    version.querySelector(".md-version__item").setAttribute("href", redirectUrl);
-    // version.querySelector(".md-version__item").addEventListener("click", function () {
-    //   console.log("Redirecting to=", redirectUrl);
-    //   window.location.href = redirectUrl;
-    // });
+    var url = version.querySelector("a").getAttribute("href")
+    const redirectUrl = url + splitUrl[1];
+    version.querySelector("a").setAttribute("href", redirectUrl);
   });
-  //document.querySelector(".md-version__item") = function () {
-  //const selectedVersion = this.value;
-  //console.log("Selected version=", selectedVersion);
-  // const currentVersion = window.location.href.split("/").slice(-2, -1)[0];
-  // console.log(currentVersion);
-  // console.log("Current URL=", window.location.href);
-  // const redirectUrl = window.location.href.replace(
-  //   `/${currentVersion}/`,
-  //   `/${selectedVersion}/`
-  // );
-  // console.log("Redirecting to=", redirectUrl)
-  //window.location.href = redirectUrl;
 });
--- v4.0/_static/js/version_redirect.js
+++ v4.0/_static/js/version_redirect.js
@@ -1,31 +1,14 @@
 window.addEventListener("DOMContentLoaded", function () {
-  versions = document.querySelectorAll(".md-version__list");
-  // Change every item in the list to point to the current page's url with the proper version
+  versions = document.querySelectorAll(".md-version__item");
+  var ABS_BASE_URL = document.baseURI;
+  var currentVersion = ABS_BASE_URL.match(/\d+\.\d+\.\d+(\-?rc\d+)?|dev/g)[0];
+  var splitUrl = ABS_BASE_URL.split(currentVersion);
+  console.log("splitUrl=", splitUrl);
   versions.forEach((version) => {
-    const currentVersion = window.location.href.split("/").slice(-2, -1)[0];
-    console.log("Current version=", currentVersion);
-    const selectedVersion = version.querySelector(".md-version__item").value;
-    console.log("Selected version=", selectedVersion);
-    const redirectUrl = window.location.href.replace(
-      `/${currentVersion}/`,
-      `/${selectedVersion}/`
-    );
+    var url = version.querySelector(".md-version__item a").getAttribute("href")
+    const redirectUrl = splitUrl[0] + currentVersion + "/" + url;
+    console.log("redirectUrl=", redirectUrl);
     version.querySelector(".md-version__item").setAttribute("href", redirectUrl);
-    // version.querySelector(".md-version__item").addEventListener("click", function () {
-    //   console.log("Redirecting to=", redirectUrl);
-    //   window.location.href = redirectUrl;
-    // });
+    console.log("changed item ", version)
   });
-  //document.querySelector(".md-version__item") = function () {
-  //const selectedVersion = this.value;
-  //console.log("Selected version=", selectedVersion);
-  // const currentVersion = window.location.href.split("/").slice(-2, -1)[0];
-  // console.log(currentVersion);
-  // console.log("Current URL=", window.location.href);
-  // const redirectUrl = window.location.href.replace(
-  //   `/${currentVersion}/`,
-  //   `/${selectedVersion}/`
-  // );
-  // console.log("Redirecting to=", redirectUrl)
-  //window.location.href = redirectUrl;
 });
