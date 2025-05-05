07e9d2af16c4915b8d4655f5bac95a4d7cb539ee
Melissa Weber Mendonça <melissawm@gmail.com>
2025-05-05 11:26:28 -0300

    WIP

--- v4.0/_static/js/version_redirect.js
+++ v4.0/_static/js/version_redirect.js
@@ -1,7 +1,7 @@
 window.addEventListener("DOMContentLoaded", function () {
   versions = document.querySelectorAll(".md-version__item");
   const currentUrl = window.location.href;
-  var currentVersion = currentUrl.match(/\/v\d+\.\d+(\-?rc\d+)?|dev\//g)[0];
+  var currentVersion = currentUrl.match(/\/v\d+\.\d+(\-?rc\d+)?\/|\/dev\//g)[0];
   var splitUrl = currentUrl.split(currentVersion);
   versions.forEach((version) => {
     var url = version.querySelector("a").getAttribute("href")
